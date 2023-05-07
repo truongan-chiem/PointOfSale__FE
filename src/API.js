cd72486be0c6d68209f5e5278edc38ae0d6f3c3b
truongan <ch.tr.an2k@gmail.comn>
2023-05-07 20:38:33 +0700

    update file env

--- src/API.js
+++ src/API.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
-
 const API = axios.create({
     //localhost
-    baseURL : "http://localhost:5000",
+    baseURL : process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_PRODUCTION : process.env.REACT_APP_API_DEVELOPMENT,
     //deploy link
     // baseURL : "https://pos-coffeeshop-be.onrender.com"
 })
